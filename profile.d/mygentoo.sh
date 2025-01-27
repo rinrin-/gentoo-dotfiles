2fac1f380c6a91a7905a72953cda96fb10e1e579
rinrin- <rinrin@imbrian.org>
2025-01-27 10:10:39 +0800

    add default profile scripts

---
+++ profile.d/mygentoo.sh
@@ -0,0 +1,13 @@
+# my gentoo login init scripts.
+#
+
+# create an alias to run yadm for system files
+alias sysyadm="sudo yadm --yadm-dir /etc/yadm --yadm-data /etc/yadm/data"
+
+if command -v portageq >/dev/null 2>&1; then
+   videocards=$(portageq envvar VIDEO_CARDS)
+   if [[ $videocards == "d3d12" ]]; then
+       GALLIUM_DRIVER=d3d12
+       export GALLIUM_DRIVER
+   fi
+fi
